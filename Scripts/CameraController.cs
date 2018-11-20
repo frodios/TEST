8ed14dcd217ad846bd42dd73d98331653c78abde
frodios <frodoinshir@mail.ru>
2018-11-20 20:02:11 +0300

    V1

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Anchor;
+    public GameObject Self;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {/*
+        Vector3 dest = Anchor.transform.position-Self.transform.position;
+        Self.*/
+	}
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontaMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontaMove= Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        animator.SetFloat("Speed",Mathf.Abs(horizontaMove));
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontaMove*Time.fixedDeltaTime, crouch, jump);
+         
+        jump = false;
+    }
+}
